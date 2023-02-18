dd7a9a8534cce561e9ec39b6b949fbf798a339eb
ostandrey <ost.and.g@gmail.com>
2023-02-18 20:13:00 +0200

    Create cart

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,14 @@ import Link from 'next/link'
 
 import {AiOutlineShopping} from 'react-icons/ai'
 
+import { Cart } from './'
+
+import { useStateContext } from 'context/StateContex'
+
 const Navbar = () => {
+
+  const { showCart, setShowCart, totalQuantities} = useStateContext();
+
   return (
     <div className='navbar-container'>
       <p className='logo'>
@@ -12,10 +19,12 @@ const Navbar = () => {
         </Link>
       </p>
 
-      <button type='button' className='cart-icon' onClick=''>
+      <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className='cart-item-qty'>1</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
+
+      {showCart && <Cart/> } 
     </div>
   )
 }
--- context/StateContex.js
+++ context/StateContex.js
@@ -1,17 +1,20 @@
 import React, { createContext, useContext, useState } from 'react'
 import { toast } from 'react-hot-toast';
+import product from 'sanity_ecommerce/schemas/product';
 
 const Context = createContext();
 
 export const StateContext = ({children}) => {
     const [showCart, setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState([]);
-    const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
-    const [qty, setQty] = useState(1)
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalQuantities, setTotalQuantities] = useState(0);
+    const [qty, setQty] = useState(1);
+
+    let foundProduct;
+    let index;
 
     const onAdd = (product, quantity) => {
-        console.log(product, quantity);
         const checkProductInCart = cartItems.find((item) => item._id === product._id);
 
         setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
@@ -36,6 +39,33 @@ export const StateContext = ({children}) => {
         toast.success(`${qty} ${product.name} added to the cart.`)
     }
 
+    const onRemove = (product) => {
+        foundProduct = cartItems.find((item) => item._id === product._id)
+        const newCartItems = cartItems.filter((item) => item._id !== product._id)
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity)
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - foundProduct.quantity)
+        setCartItems(newCartItems)
+    }
+
+    const toggleCartItemQuantity = (id, value) => {
+        foundProduct = cartItems.find((item) => item._id === id)
+        index = cartItems.findIndex((product) => product._id === id)
+        const newCartItems = cartItems.filter((item) => item._id !== id)
+
+        if(value === 'inc') {
+            setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1}])
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1)
+        } else if (value === 'dec') {
+            if(foundProduct.quantity > 1) {
+                setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1}])
+                setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+                setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1)
+            }
+        }
+    }
+
     const incQty = () => {
         setQty((prevQty) => prevQty + 1)
     }
@@ -53,13 +83,16 @@ export const StateContext = ({children}) => {
         <Context.Provider
             value={{
                 showCart,
+                setShowCart,
                 cartItems,
                 totalPrice,
                 totalQuantities,
                 qty,
                 incQty,
                 decQty,
-                onAdd
+                onAdd,
+                onRemove,
+                toggleCartItemQuantity
             }}
         >
             {children}
