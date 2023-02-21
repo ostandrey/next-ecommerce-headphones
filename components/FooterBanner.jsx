02ece3ea4d5e0ff131c9c4f23b4080fc48428888
ostandrey <ost.and.g@gmail.com>
2023-02-21 14:05:59 +0200

    Update styles

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -23,7 +23,7 @@ const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTi
           </Link>
         </div>
 
-        <img src={urlFor(image)} className='footer-bbanner-image'/>
+        <img src={urlFor(image)} className='footer-banner-image'/>
       </div>
     </div>
   )
